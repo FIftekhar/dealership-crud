fab16a4ac0cbe42572730dbeec7cd8dba6d3c6a7
fi375 <fi375@nyu.edu>
2025-04-06 21:08:04 -0400

    new ignore

--- car_dealership_project/car_dealership_project/urls.py
+++ car_dealership_project/car_dealership_project/urls.py
@@ -1,34 +1,8 @@
-# dealership/urls.py
-from django.urls import path
-from . import views
+# car_dealership_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # Home page
-    path('', views.home, name='home'),
-    
-    # Customer views
-    path('customers/', views.customer_list, name='customer_list'),
-    path('customers/new/', views.customer_create, name='customer_create'),
-    path('customers/<int:pk>/', views.customer_detail, name='customer_detail'),
-    path('customers/<int:pk>/edit/', views.customer_update, name='customer_update'),
-    path('customers/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
-    
-    # Vehicle views
-    path('vehicles/', views.vehicle_list, name='vehicle_list'),
-    path('vehicles/new/', views.vehicle_create, name='vehicle_create'),
-    path('vehicles/<int:pk>/', views.vehicle_detail, name='vehicle_detail'),
-    path('vehicles/<int:pk>/edit/', views.vehicle_update, name='vehicle_update'),
-    path('vehicles/<int:pk>/delete/', views.vehicle_delete, name='vehicle_delete'),
-    
-    # Employee views
-    path('employees/', views.employee_list, name='employee_list'),
-    path('employees/new/', views.employee_create, name='employee_create'),
-    path('employees/<int:pk>/', views.employee_detail, name='employee_detail'),
-    path('employees/<int:pk>/edit/', views.employee_update, name='employee_update'),
-    path('employees/<int:pk>/delete/', views.employee_delete, name='employee_delete'),
-    
-    # Transaction views
-    path('transactions/', views.transaction_list, name='transaction_list'),
-    path('transactions/new/', views.transaction_create, name='transaction_create'),
-    path('transactions/<int:pk>/', views.transaction_detail, name='transaction_detail'),
+    path('admin/', admin.site.urls),
+    path('', include('dealership.urls')),
 ]
